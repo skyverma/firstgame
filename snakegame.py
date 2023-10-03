954cae3382fc80379e883d186a4b3f9193b3a873
shiv-pencia <146872972+shiv-pencia@users.noreply.github.com>
2023-10-03 23:34:59 +0530

    Update snakegame.py
    follow ke le nhi to aau main

--- snakegame.py
+++ snakegame.py
@@ -1,7 +1,7 @@
 import pygame
 import random
 x=pygame.init()
-#Creating window
+#Creating window bsdke follow kr le mn
 game_window=pygame.display.set_mode((1200,500))
 pygame.display.set_caption('MY first game')
 pygame.display.update()
